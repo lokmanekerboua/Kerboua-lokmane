b24eaf6ab20327701b8c3fc5de4b4bb1b22f8a73
Lokmane Kerboua <Lokmankerboua@gmail.com>
2025-10-19 00:01:45 +0200

    changes and add french language

--- src/components/CV/cv.jsx
+++ src/components/CV/cv.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from "./Cv.module.css";
 import {useTheme} from "../../ThemeContext.jsx";
+import {useLanguage} from "../../LanguageContext.jsx";
 
 export const Cv = () => {
     const cvLinkEN = "https://drive.google.com/file/d/1uGBQu9IRsXPKGbL-HmQdJfSOB2Lgtp0_/view?usp=drive_link";
     const cvLinkFR = "https://drive.google.com/file/d/1XD1PSEyXn7phjk-tmjRIMSZXgPpJTjqq/view?usp=drive_link";
-    const {isDark, toggleTheme} = useTheme();
+    const {isDark} = useTheme();
+    const {language} = useLanguage();
+    const downloadText = language === 'fr' ? 'Télécharger le CV' : 'Download CV';
+    const frCV = language==='fr'? 'Français' : 'French';
+    const enCV = language==='fr'? 'Anglais' : 'English';
     return (
         <section className={styles.container}>
             <div className={styles.content}>
@@ -13,13 +18,13 @@ export const Cv = () => {
                 <div className={isDark ? styles.btnContainerDark : styles.btnContainer}>
                         <a href={cvLinkEN} download="Your_CV_Name.pdf" target={"_blank"}
                            className={isDark ? styles.contactBtnDark : styles.contactBtn}>
-                            Download CV (English)
+                            {downloadText} ({enCV})
                         </a>
                 </div>
                 <div className={isDark ? styles.btnContainerDark : styles.btnContainer}>
                         <a href={cvLinkFR} download="Your_CV_Name.pdf" target={"_blank"}
                            className={isDark ? styles.contactBtnDark : styles.contactBtn}>
-                            Download CV (Français)
+                            {downloadText} ({frCV})
                         </a>
                 </div>
             </div>
